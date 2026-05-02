e3f8ef34922fdda5cd67a5acec260773a9e81c9f
rab <beluga8787askerov@gmail.com>
2026-05-03 00:56:17 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,109 @@
+export const products = [
+    //рыбное:
+    {
+        type: 'riba',
+        name: 'семга-тунец маслянная',
+        image: './riba/semtunmas.jpg'
+    },
+    {
+        type: 'riba',
+        name: 'Шампур',
+        image: './riba/shampur.jpg'
+    },
+    {
+        type: 'riba',
+        name: 'Мидий',
+        image: './riba/midiy.jpg'
+    },
+    {
+        type: 'riba',
+        name: 'Креветки чищенные',
+        image: './riba/krevetchish.jpg'
+    },
+    {
+        type: 'riba',
+        name: 'Креветки жаренные',
+        image: './riba/krevetjar.jpg'
+    },
+    {
+        type: 'riba',
+        name: 'Лангустины',
+        image: './riba/langust.jpg'
+    },
+    {
+        type: 'riba',
+        name: 'Осминоги',
+        image: './riba/osminogi.jpg'
+    },
+
+    //мясное:
+    {
+        type: 'meat',
+        name: 'Перепелка',
+        image: './meats/perepel.jpg'
+    },
+    {
+        type: 'meat',
+        name: 'Крылышки',
+        image: './meats/krilya.jpg'
+    },
+    {
+        type: 'meat',
+        name: 'Чипсы - палочки',
+        image: './meats/chips.jpg'
+    },
+    {
+        type: 'meat',
+        name: 'Сердечки',
+        image: './meats/serdech.jpg'
+    },
+    {
+        type: 'meat',
+        name: 'Венгерские',
+        image: './meats/veng.jpg'
+    },
+    {
+        type: 'meat',
+        name: 'Баварские',
+        image: './meats/bavar.jpg'
+    },
+    {
+        type: 'meat',
+        name: 'Стейк индейки',
+        image: './meats/steak.jpg'
+    },
+
+    //остальное
+    {
+        type: 'other',
+        name: 'Сыр жаренный',
+        image: './other/cheesejar.jpg'
+    },
+    {
+        type: 'other',
+        name: 'Кальмар',
+        image: './other/kalmar.jpg'
+    },
+    {
+        type: 'other',
+        name: 'Гренки',
+        image: './other/grenki.jpg'
+    },
+    {
+        type: 'other',
+        name: 'Деревня',
+        image: './other/derev.jpg'
+    },
+    {
+        type: 'other',
+        name: 'Картошка фри',
+        image: './other/fri.jpg'
+    },
+
+    //наборы
+    {
+        type: 'nabor',
+        name: 'Биг сет',
+        image: './nabor/nabor1.jpg'
+    },
+]
---
+++ nabor.js
@@ -0,0 +1,35 @@
+import { products } from "./data.js"
+
+function showProducts() {
+    for(let prad of products) {
+        
+        if(prad.type === "nabor") {
+            const card = document.createElement('div');
+            card.className = 'card';
+    
+            const cardEl = document.createElement('div');
+            cardEl.className = 'card-el';
+            
+            const img = document.createElement('img');
+            img.className = 'card-img';
+    
+            const productName = document.createElement('p');
+            productName.className = 'card-title';
+    
+            img.src = prad.image;
+            productName.textContent = prad.name;
+            cardEl.append(productName);
+            card.append(img, cardEl);
+            document.querySelector('.products').append(card);
+
+        }
+
+        
+    } 
+}
+
+function main() {
+    showProducts();
+}
+
+main();
